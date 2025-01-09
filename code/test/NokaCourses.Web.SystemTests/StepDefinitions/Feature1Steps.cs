8356d4a0afe089f84870c75e5e4f154058b3bfd2
noor khan <nrkhan96@outlook.com>
2025-01-09 11:02:56 +0000

    initial

---
+++ code/test/NokaCourses.Web.SystemTests/StepDefinitions/Feature1Steps.cs
@@ -0,0 +1,25 @@
+﻿using TechTalk.SpecFlow;
+using Xunit;
+
+namespace NokaCourses.Web.SystemTests.StepDefinitions
+{
+    [Binding]
+    public class PlayerCharacterSteps
+    {
+        [Given(@"I'm a new player")]
+        public void GivenImANewPlayer()
+        {
+        }
+
+        [When(@"I take 0 damage")]
+        public void WhenITake0Damage()
+        {
+        }
+
+        [Then(@"My health should now be 100")]
+        public void ThenMyHealthShouldNowBe100()
+        {
+            Assert.Equal(1,1);
+        }
+    }
+}
